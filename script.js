062a0a7981ee9e7f0774064bd69aa2fee15fe273
Prashant Salve <128687203+prashant04071995@users.noreply.github.com>
2024-03-01 12:31:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        // Leap year
+        return 366;
+    } else {
+        // Non-leap year
+        return 365;
+    }
+}
